c4befa5f6756c62f1741b4e014d32907aa3b0992
hiraok <hiraok.work@gmail.com>
2023-01-27 20:11:59 +0900

    firstcommi

--- HttpTrigger1/index.ts
+++ HttpTrigger1/index.ts
@@ -2,13 +2,12 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const responseMessage = req.body
+    const response = req.body.split("&")
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: response[0]
     };
-
-
 };
 
 export default httpTrigger;
