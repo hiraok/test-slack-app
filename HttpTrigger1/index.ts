a65cb0d56985d0fa0023fbae7195208baa9a6d22
hiraok <hiraok.work@gmail.com>
2023-02-12 14:19:04 +0900

    point3

--- HttpTrigger1/index.ts
+++ HttpTrigger1/index.ts
@@ -1,11 +1,11 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const splitValue = req.body.split("&").map(v => v.split("="));
-    let result = {};
+    context.log('HTTP trigger function processed a request.')
+    const splitValue = req.body.split("&").map(v => v.split("="))
+    const result = {};
     splitValue.forEach((element) => {
-        result[element[0]] = element[1];
+        result[element[0]] = decodeURIComponent(element[1])
     })
     context.res = {
         // status: 200, /* Defaults to 200 */
